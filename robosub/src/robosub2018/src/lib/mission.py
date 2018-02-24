1414f77e681b1ffb4f73a989b3f7ba490ed3d492
Kirby Overman <kirby.overman@gmail.com>
2018-02-23 23:25:14 -0700

    mission refactoring

--- robosub/src/robosub2018/src/lib/mission.py
+++ robosub/src/robosub2018/src/lib/mission.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python
 
 import json
+from tasks import *
 from robosub2018.msg import State, Detection
 
 class Mission():
 
-    def __init__(self, initial_mission):
-        self.queue = []
-        self.current = initial_mission
+    def __init__(self, initial_mission = 'prequalification'):
+        self.queue = [] # Priority Queue - Tasks to complete after current
+        self.current = initial_mission # Active Task - Want the object
+        self.completed = []
+        
+        tasks_file = open('tasks.json')
+        tasks_string = tasks_file.read()
+        self.tasks = json.loads(tasks_string)
+
+    def get_next(self):
+        self.completed.append(self.current.priority)
+        self.current = heappop(self.queue)
 
 
-    def check_mission():
-        pass
-
+    
 

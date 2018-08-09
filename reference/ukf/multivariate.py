45865dda8a3f2d1b1bc119912a1b13cda77c9715
Kirby <knoverman@comcast.net>
2018-08-08 20:29:36 -0700

    gaussian acceleration measurements

--- reference/ukf/multivariate.py
+++ reference/ukf/multivariate.py
@@ -29,22 +29,28 @@ if __name__ == "__main__":
     acceleration    = np.array([1., 0.]) # randomly set ~N(0, 1)
     variance        = np.matlib.identity(2, dtype=float) * 0.01
     measurement     = np.array([0., 0.]) # randomly set ~N(position, 1)
+    true_acceleration = np.array([1., 0.])
+    true_velocity   = np.array([0., 0.])
+    true_final_velocity = np.array([0., 0.])
     true_position   = np.array([0., 0.])
 
     ukf = UKF(R, Q, G, H)
 
    
     for i in range(1, 11):
-        acceleration = np.array([gauss(1, 0.1), gauss(1, 0.1)])
-        #final_velocity = np.array([velocity[j] + (delta_t * acceleration[j]) for j in range(len(position))])
+        acceleration = np.array([gauss(true_acceleration[0], 0.1), gauss(true_acceleration[1], 0.1)])
         final_velocity = np.array([velocity[j] + (delta_t * acceleration[j]) for j in range(len(position))])
-        true_position = np.array([true_position[l] + delta_t * ((final_velocity[l] + velocity[l]) / 2) for l in range(len(position))])
-        measurement = np.array([gauss(true_position[k], 0.1) for k in range(len(position))])
+        #measurement = np.array([gauss(true_position[k], 0.1) for k in range(len(position))])
+        measurement = np.array([true_position[k] + delta_t * ((final_velocity[k] + velocity[k]) / 2) for k in range(len(position))])
 
-        position, variance = ukf.get_belief(position, variance, measurement, (final_velocity + velocity) / 2)
+        true_final_velocity = np.array([true_velocity[j] + (delta_t * true_acceleration[j]) for j in range(len(true_position))])
+        true_position = np.array([true_position[l] + delta_t * ((true_final_velocity[l] + true_velocity[l]) / 2) for l in range(len(true_position))])
+
+        position, variance = ukf.get_belief(position, variance, measurement, final_velocity)
         error = true_position - position
         m_error = true_position - measurement
         velocity = np.copy(final_velocity)
+        true_velocity = np.copy(true_final_velocity)
         
         print("Time: %s" % i)
         print("Acceleration: %s" % acceleration)
